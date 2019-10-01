7cc5fa5c5cb98f26fd2ad7f96b557b9e9758b314
MrWonsik <mrt.wonsik@gmail.com>
2019-10-01 22:15:17 +0200

    new file for const request method

---
+++ src/RequestMethod.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const API = "http://192.168.0.101:8080/planner/";
+export const GET_madeTasks = 'getMadeTasks';
+export const GET_tasksToDo = 'getTasksToDo';
+export const POST_addTask = 'addTask';
+export const DELETE_deleteTask = 'deleteTask/';
+export const PUT_completeTask = 'completeTask/';
+export const PUT_undoCompleteTask = 'undoCompleteTask/';
+
+export default RequestMethod;
